c43de6aa8b420313ff1322232bda7eb0e7a36937
Jonas Eltes <jonas@eltes.se>
2016-06-27 15:22:01 +0200

    Disabled controls

--- js/content.js
+++ js/content.js
@@ -118,6 +118,9 @@ function init() {
     //controls.damping = 0.2;
     controls.update();
     console.log("controls: " ,controls);
+    controls.enabled = false;
+    // controls.enableRotate = false;
+    // controls.enablePan = false;
     //controls.object.position.y = 50;
     //var targetV = new THREE.Vector3(0, 5, 0);
     
